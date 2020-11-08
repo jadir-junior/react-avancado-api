63c08019ab09adaf8f574ac32e4b84b09f74bbd6
Jadir Junior <jadirjsjunior@gmail.com>
2020-11-08 14:13:03 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '31323494b628fc5670b0985c7e27f856'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0d67f6bb-58f3-4dac-baa0-e16124085f08'
+};
